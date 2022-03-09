6af386557dc941547fbbc7535cb1af5c804e6072
Twuebker <theo@jmop.de>
2022-03-09 06:40:40 -0500

    pls fix

---
+++ build/CMakeFiles/3.10.2/CompilerIdCUDA/tmp/CMakeCUDACompilerId.cudafe1.c
@@ -0,0 +1,25 @@
+# 1 "CMakeCUDACompilerId.cu"
+# 41 "CMakeCUDACompilerId.cu"
+extern const char *info_compiler;
+# 233 "CMakeCUDACompilerId.cu"
+static const char info_version[50];
+# 280 "CMakeCUDACompilerId.cu"
+extern const char *info_platform;
+extern const char *info_arch;
+
+
+
+
+extern const char *info_language_dialect_default;
+# 41 "CMakeCUDACompilerId.cu"
+const char *info_compiler = ((const char *)"INFO:compiler[NVIDIA]");
+# 233 "CMakeCUDACompilerId.cu"
+static const char info_version[50] = {((char)73U),((char)78U),((char)70U),((char)79U),((char)58U),((char)99U),((char)111U),((char)109U),((char)112U),((char)105U),((char)108U),((char)101U),((char)114U),((char)95U),((char)118U),((char)101U),((char)114U),((char)115U),((char)105U),((char)111U),((char)110U),((char)91U),((char)48U),((char)48U),((char)48U),((char)48U),((char)48U),((char)48U),((char)49U),((char)48U),((char)46U),((char)48U),((char)48U),((char)48U),((char)48U),((char)48U),((char)48U),((char)48U),((char)50U),((char)46U),((char)48U),((char)48U),((char)48U),((char)48U),((char)48U),((char)51U),((char)48U),((char)48U),((char)93U),((char)0U)};
+# 280 "CMakeCUDACompilerId.cu"
+const char *info_platform = ((const char *)"INFO:platform[Linux]");
+const char *info_arch = ((const char *)"INFO:arch[]");
+
+
+
+
+const char *info_language_dialect_default = ((const char *)"INFO:dialect_default[14]");
