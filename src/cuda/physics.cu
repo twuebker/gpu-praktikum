efeffd1404dcfa56c75de527f2e8b7bab92f1a13
leonardfranke <leonard.franke@t-online.de>
2022-03-09 11:27:09 +0100

    ok

--- src/cuda/physics.cu
+++ src/cuda/physics.cu
@@ -1,17 +1,21 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <vector>
-#include <AsteroidsScene.h>
+#include "../widgets/AsteroidsScene.h"
 
-__global__ void calc_asteroid_physics(void) {
+__global__ void calculate_forces(Asteroid *asteroids) {
 	
 }
 
-float call_kernel(std::vector<Asteroid>& asteroids) {
-	Asteroid* a = asteroids.data();
-    Asteroid* dA;
-    cudaMalloc(&da, sizeof(Asteroid) * asteroids.size())
-	calc_asteroid_physics<<<1, 1>>>();
+float call_kernel() {
+	int n = 4;
+	float* h_p = (float*)malloc(4 * sizeof(float) * n);
+	float* h_a = (float*)malloc(3 * sizeof(float) * n);
+
+	float* d_p;
+	float* d_a;
+	cudaMalloc(&d_p, 4 * sizeof(float) * n);
+	cudaMalloc(&d_a, 4 * sizeof(float) * n);
 
 	
 }
